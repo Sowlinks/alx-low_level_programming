650ade68755ded98bf5cd728c22d58d66e402e80
kurolinks <wsubiri@gmail.com>
2023-07-13 19:58:18 +0000

    write program for file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string in the put function
+ * Description: using the main function
+ * program prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
+*/
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
