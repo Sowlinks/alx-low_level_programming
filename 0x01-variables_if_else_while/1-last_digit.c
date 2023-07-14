09a51ac580ba267d4a2e2273e1dc95902d17b1d3
kurolinks <wsubiri@gmail.com>
2023-07-14 21:57:35 +0000

    file 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 
 	if (m > 5)
 	{
-		printf("Last digit of %d is %i and is greater than 5/n", n, m);
+		printf("Last digit of %d is %i and is greater than 5\n", n, m);
 	}
 	else if (m == 0)
 		printf("Last digit of %d is %i and is 0\n", n, m);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets
+ * Description: prints alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n;
+
+	n == 'a';
+
+	while (n < 'z')
+	{
+		n++;
+		putchar(n);
+	}
+
+
+	return (0);
+}
