ecdbe338847fdb214fc1e0429afb6de514ab7a4f
kurolinks <wsubiri@gmail.com>
2023-07-14 23:06:13 +0000

    file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ * Description: prints single digit of base 10 numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+#
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
