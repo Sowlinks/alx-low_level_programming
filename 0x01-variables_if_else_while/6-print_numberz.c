dc92d978ec44dbe969b8a619ce149289c3d5f16d
kurolinks <wsubiri@gmail.com>
2023-07-14 23:25:05 +0000

    file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ * Description: prints single digit of base 10 numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 9)
+	{
+
+	printf("%d", n);
+
+		n++;
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
+
