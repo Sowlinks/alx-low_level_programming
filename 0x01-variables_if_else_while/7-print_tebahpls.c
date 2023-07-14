22022bea794fca3bcc1ebe4ac62f0db50af269bd
kurolinks <wsubiri@gmail.com>
2023-07-14 23:50:32 +0000

    file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpls.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets
+ * Description: prints alphabet in lower case in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
