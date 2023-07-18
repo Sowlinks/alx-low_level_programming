e7cc8ba1d99f41fe5e6f3c16177e28a2a3efcaae
kurolinks <wsubiri@gmail.com>
2023-07-18 09:37:44 +0000

    add two directories

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints to standard error
+ * Description: A program that prints an exact sentence to
+ * the standard error
+ * Return: 1
+ */
+
+int main(void)
+{
+	char message[] = ("and that piece of art is useful\" - Dora Korpar, 2015- 10-19");
+	putchar('\n');
+	printf(message)
+
+	return (1);
+}
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+*A Repository for degugging tasks
