bf5632bcf8ad50601950eea455302c63f6d5c5ca
kurolinks <wsubiri@gmail.com>
2023-07-19 21:30:57 +0000

    edit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,16 @@ void print_to_98(int n)
 	while (n < 98)
 	{
 		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
 		n++;
 	}
 
 	while (n > 98)
 	{
 		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
 		n--;
 	}
-	_putchar(',');
-	_putchar(' ');
 }
