4c946bb1942d0f0c98441212e02bb0d207e45d25
kurolinks <wsubiri@gmail.com>
2023-07-19 20:55:50 +0000

    add file 10

---
+++ 0x02-functions_nested_loops/10-acc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Description: a function that adds two numbers
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum_total = a * b;
+
+	return (sum_total);
+}
