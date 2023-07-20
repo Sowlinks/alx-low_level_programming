b89a40a5655230f807c2c05e2a11cdd326c82b2b
kurolinks <wsubiri@gmail.com>
2023-07-20 21:36:01 +0000

    add file 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line- prints line
+ * @n - the line variable
+ * Description: a function that prints a straight line
+ * Return: 0
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
