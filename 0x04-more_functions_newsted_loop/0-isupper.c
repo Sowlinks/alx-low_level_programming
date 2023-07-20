ee24f689e02f20b3216bd27da6889dc150f8098f
kurolinks <wsubiri@gmail.com>
2023-07-20 13:00:47 +0000

    add a new directory

---
+++ 0x04-more_functions_newsted_loop/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * isupper- : checks for upper case
+ * @c: variable for the character to be checked
+ * Description: a function that checks if a character is in uppercae
+ * Return: 0
+ */
+int isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
