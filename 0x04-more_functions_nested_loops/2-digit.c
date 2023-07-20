d2e081743e321705a9b1e9ac3fb81bb9f83ca84d
kurolinks <wsubiri@gmail.com>
2023-07-20 14:16:56 +0000

    add file 2

---
+++ 0x04-more_functions_nested_loops/2-digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul- multiplication function
+ * @a: first number
+ * @b: second number
+ * Descriptipn: a function that multiplies two numbers
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+	return (product);
+}
