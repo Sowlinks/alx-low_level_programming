4d33d8ab7e11d6b4a1483fcb58a0eafc74f69a22
kurolinks <wsubiri@gmail.com>
2023-07-20 14:33:52 +0000

    add file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- print 0 - 9
+ * Decription: a function that prints
+ * from 0 to 9
+ * Return: (0)
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -27,4 +27,6 @@ void print_square(int size);
 
 void print_triangle(int size);
 
+int _putchar(char);
+
 #endif
