88b6efde45f306498953c7de710f32fd2a18ad67
kurolinks <wsubiri@gmail.com>
2023-07-22 16:31:31 +0000

    add file 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table- function for times table
+ * Description: a function that prints the times table
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i, n, total;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			total = i * n;
+			printf("%d", total);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
