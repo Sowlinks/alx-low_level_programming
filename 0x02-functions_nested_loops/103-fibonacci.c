b707974f932d8a4c4ef9d4e1645eff90a18a7b86
kurolinks <wsubiri@gmail.com>
2023-07-23 15:21:38 +0000

    file 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main- fibonacci program
+ * Description: a program that prints the sum of the even numbers
+ * of the fibonacci sequence
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long a, b, rsum, sum;
+	int n;
+
+	a = 1;
+
+	b = 2;
+
+	sum = 0;
+
+	for (n = 0; n <= 47; n++)
+	{
+		rsum = a + b;
+
+		if (rsum % 2 == 0 && rsum)
+			sum = sum += rsum;
+
+		a = b;
+
+	b = rsum;
+
+	}
+	sum = sum + 2;
+
+	printf("%lu", sum);
+	printf("\n");
+	return (0);
+}
