68f7b3abc492aaf0d10e267b6838567be102789c
kurolinks <wsubiri@gmail.com>
2023-07-23 15:38:51 +0000

    add file 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main- fibonacci program
+ * Description: a function that prints the first 98 fibonnaci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long a, b, rsum;
+	int n;
+
+	a = 1;
+
+	b = 2;
+
+	printf("%lu %lu ", a, b);
+	for (n = 0; n <= 95; n++)
+	{
+		rsum = a + b;
+
+		a = b;
+
+		b = rsum;
+		printf("%lu ", rsum);
+	}
+	return (0);
+}
