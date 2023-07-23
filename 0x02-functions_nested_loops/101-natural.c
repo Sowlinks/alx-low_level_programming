20281ea171c2ec68103b1cd96f29248ea23e0ff7
kurolinks <wsubiri@gmail.com>
2023-07-23 08:49:00 +0000

    edit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,8 +11,9 @@ int main(void)
 	int n, sum;
 
 	for (n = 1; n < 1024; n++)
-		if ((n & 3 == 0 || n % 5 == 0))
+		if ((n % 3 == 0 || n % 5 == 0))
 			sum += n;
 
 	printf("%d", sum);
+	return (0);
 }
