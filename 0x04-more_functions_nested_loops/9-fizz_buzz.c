e5700862f89c9a5c16e3a59cfd4445ce6e9261b3
kurolinks <wsubiri@gmail.com>
2023-07-23 20:44:15 +0000

    edit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,18 @@ int main(void)
 	for (n = 1; n < 101; n++)
 	{
 		if (n % 3 == 0)
-			printf(" Fizz");
+			if (n % 5 == 0)
+				printf(" FizzBuzz");
+			else
+				printf(" Fizz");
 		else if (n % 5 == 0)
-			printf(" Buzz");
-		else if (n % 3 == 0 && n % 5 == 0)
-			printf(" FizzBuzz");
+			if (n % 3 == 0)
+				printf(" FizzBuzz");
+			else
+				printf(" Buzz");
 		else
 			printf(" %d", n);
 	}
 
-	printf("\n");
 	return (0);
 }
