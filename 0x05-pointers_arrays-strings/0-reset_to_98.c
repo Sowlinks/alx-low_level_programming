811d8803bc559e9c119656b4fd5f311bc4e79dee
kurolinks <wsubiri@gmail.com>
2023-07-24 18:02:38 +0000

    add new directory

---
+++ 0x05-pointers_arrays-strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98- pointer that updates value
+ * @n: variable
+ * Description; a function that takes an int and updates it value to 98
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays-strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main- header file for prototypes
+ * int prototype (ubt)
+ * void prototype (void)
+ */
+
+void reset_to_98(int *n);
+
+#endif
