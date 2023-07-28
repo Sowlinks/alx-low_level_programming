d0d9d4c463cc24520363e875f9c9ba05a0f94e65
kurolinks <wsubiri@gmail.com>
2023-07-28 21:17:03 +0000

    edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,11 @@ char *_strcat(char *dest, char *src)
 	int n;
 
 	for (n = 0; n < strlen(src); n++)
+	{
 		printf("%c", src[n]);
 		for (n = 0; n < strlen(dest); n++)
+		{
 			printf("%c", dest[n]);
+		}
+	}
 }
