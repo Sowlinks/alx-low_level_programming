0f9b061239508bd7ac06208d6bf9d5997ba999ef
kurolinks <wsubiri@gmail.com>
2023-08-02 23:26:00 +0000

    file 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,20 +1,35 @@
 #include "main.h"
+#include <stdio.h>
+#include "_putchar.c"
 
 /**
- * _print_rev_recursion- recursion function
- * @s: string to be printed
- * Description: a function that prints a string
+ * _strlen- recursion function
+ * @s: string to be checked
+ * Description: a functiion that returns the legnth of a string
  * Return: 0;
  */
 
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return;
+		return(0);
 	}
 	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
+	return (1 + _strlen_recursion(s));
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
 }
