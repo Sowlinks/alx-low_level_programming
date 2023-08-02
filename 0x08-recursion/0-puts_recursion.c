d10de3960eace8590d314e0e70603e91e07697c1
kurolinks <wsubiri@gmail.com>
2023-08-02 21:54:35 +0000

    file 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- recursion function
+ * @s: string to be printed
+ * Description: a function that prints a string
+ * Return: 0;
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+		return;
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
