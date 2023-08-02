713b59668481327f58474669589057db4cdb8416
kurolinks <wsubiri@gmail.com>
2023-08-02 23:56:32 +0000

    edit file 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen- recursion function
+ * _strlen_recursion - recursion function
  * @s: string to be checked
  * Description: a functiion that returns the legnth of a string
  * Return: 0;
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	s++;
 	return (1 + _strlen_recursion(s));
