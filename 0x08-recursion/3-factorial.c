975b2146944832922ecc876054f13e3dd8e231fa
kurolinks <wsubiri@gmail.com>
2023-08-03 00:05:58 +0000

    file 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
-#include <stdio.h>
 
 /**
  * factorial- factorial function
@@ -13,25 +11,4 @@ int factorial(int n)
 	if (n < 0)
 		return (-1);
 	return (n + factorial(n - 1));
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	int r;
-
-	r = factorial(1);
-	printf("%d\n", r);
-	r = factorial(5);
-	printf("%d\n", r);
-	r = factorial(10);
-	printf("%d\n", r);
-	r = factorial(-1024);
-	printf("%d\n", r);
-	return (0);
 }
