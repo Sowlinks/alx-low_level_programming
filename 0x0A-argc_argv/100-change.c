7d51dceba00ec426a21d855e30a8f820f2cff07e
kurolinks <wsubiri@gmail.com>
2023-08-09 11:48:39 +0000

    edit final file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,28 +2,36 @@
 #include <stdlib.h>
 
 /**
- * main- multiplies two numbers
- * @argc: count of arguments
- * @argv: strings passed to the program
- * Description: a program that multiplies two numbers and
- * prints its output
- * Return: always return 0
- */
-
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
 int main(int argc, char *argv[])
 {
-	int result, x, y;
+	int num, j, result = 0;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc < 3)
+	if (argc != 2)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
 
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	result = x * y;
-
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			num -= coins[j];
+			result++;
+		}
+	}
 	printf("%d\n", result);
 	return (0);
 }
