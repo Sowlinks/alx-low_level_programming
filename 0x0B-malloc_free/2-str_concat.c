6780f1fa61ce7f84ec210058ac3893313bf2e0d7
kurolinks <wsubiri@gmail.com>
2023-08-12 09:28:16 +0000

    edit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,10 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	/*checks if strings are empty*/
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	/*n signigies the legnth of the string*/
 	while (s1[n] != '\0')
