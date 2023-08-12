96350b6ad2f7208eb24592a394af7bd896d6168e
kurolinks <wsubiri@gmail.com>
2023-08-12 15:20:55 +0000

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints to standard error
@@ -9,10 +10,6 @@
 
 int main(void)
 {
-	char message[] = ("and that piece of art is useful\" - Dora Korpar, 2015- 10-19");
-	putchar('\n');
-	printf(message);
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
-
