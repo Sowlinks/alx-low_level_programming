adb71f02a7bc8bd20cc4c9ca781da4bf40159a3c
kurolinks <wsubiri@gmail.com>
2023-08-18 15:00:34 +0000

    compile file 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,5 +13,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d = {jimmu, 2, "Paul"};
+	if (d != NULL)
+	{
+		struct dog d = {"Jimmy", 2, "Paul"};
+	}
 }
