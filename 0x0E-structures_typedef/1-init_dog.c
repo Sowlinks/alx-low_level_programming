7081fb163249791e673b73f28d02ac6e39b34b25
kurolinks <wsubiri@gmail.com>
2023-08-18 14:53:33 +0000

    file 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+* ini_dog: a function that initializes a variable of type struct dog
+* struct dog: a structure data type that defines a dog
+* @d: member d
+* @name: name of the dog
+* @age: dog's age
+* @owner: owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d = {jimmu, 2, "Paul"};
+}
