2d291505145733e142754b9621fc1f53c4072bc3
kurolinks <wsubiri@gmail.com>
2023-08-18 14:38:29 +0000

    add a new directory for c structures

---
+++ 0x0E-atructutes_typedef/.gitignore
@@ -0,0 +1,4 @@
+#ignore file with ain.c extensions, _puthar.c and a.out
+*ain.c
+a.out
+_putchar.c
---
+++ 0x0E-atructutes_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* struct dog - a structure data type that defines a dog
+* @name: name of the dog
+* @age: dog's age
+* @owner: owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char[] *owner;
+};
+
+#endif
