2b48bdb9c6802bbd2cd5624b4dfd1a21a9e1280f
kurolinks <wsubiri@gmail.com>
2023-08-30 14:41:33 +0000

    work on the print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,5 +9,6 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
+	if (s != '\0')
+		_putchar(*s++);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 int _putchar(char);
-
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
