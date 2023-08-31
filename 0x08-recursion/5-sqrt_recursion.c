93c97c85177fa53fe87d3595f2980a40ed6ad241
kurolinks <wsubiri@gmail.com>
2023-08-31 13:46:57 +0000

    work on square root function

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion- square rooot function
+ * @n: member n
+ * Description: a function to return the natural square root of a number
+ * Return: returns square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	_sqrt(n, 1);
+}
+
+/**
+ * _sqrt- performs the square root
+ * @n: number
+ * @x: iterative member
+ * Return: retun the square root
+ */
+
+int _sqrt(int n, int x)
+{
+	int square = x * x;
+
+	if (square == n)
+	{
+		return (square);
+	}
+	if (square > n)
+	{
+		return (-1);
+	}
+	return (_sqrt(n, x + 1));
+}
