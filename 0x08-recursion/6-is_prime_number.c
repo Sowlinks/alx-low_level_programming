c36fe964c7d6149e258c8b8ea8acaa1624369fb7
kurolinks <wsubiri@gmail.com>
2023-08-31 14:38:57 +0000

    work on prime number function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _prime-: prime number function
+ * @n: number to be checked
+ * @x: number to iterate
+ * Description: function for checking the prime
+ * Return: returns prime
+ */
+
+int _prime(int n, int x)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+
+	if (n % x == 0 && x > 1)
+	{
+		return (0);
+	}
+
+	if ((n / x) < x)
+	{
+		return (1);
+	}
+
+	return (_prime(n, x + 1));
+}
+
+
+/**
+ * is_prime_number- prime number function
+ * @n: number to check
+ * Description: function to return prime number
+ * Return: returns prime
+ */
+
+int is_prime_number(int n)
+{
+	return (_sqrt(n, 1));
+}
