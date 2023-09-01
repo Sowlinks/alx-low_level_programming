21e59954773e8ca0f3429d9b7e913228805f7e00
kurolinks <wsubiri@gmail.com>
2023-09-01 18:29:38 +0000

    edit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,24 +12,72 @@
 
 int main(int argc, char *argv[])
 {
-	int r1, r2;
+	int i, r1, r2, r3, r4, r5, r6, p1, p2, p3, p4;
+	int cents[] = {25, 10, 5, 2, 1};
+	int total;
 
 	if (argc != 2)
 	{
-		printf("Error\n");
+		printf("Error");
 		return (1);
 	}
 
-	if (atoi(argv[1])%25 == 0)
+	if (atoi(argv[1] <= (cents[0]);
 	{
-		r1 = atoi(argv[1])/25;
-		printf("%d\n", r1);
+		r1 = atoi(argv[1]) % (cents[0]);
+		r2 = atoi(argv[1]) / (cents[0]);
+
+		if (r1 == 0)
+		{
+			total 
+			printf("%i\n", total);
+			return (1);
+		}
 	}
 
-	else
+	 if (atoi(argv[1] <;wq= (cents[0]);
 	{
-		r2 = atoi(argv[1])/5;
-		r1 += r2;
-		printf("%d\n", r2);
+		r3 = atoi(argv[1]) % (cents[1]);
+		r4 = atoi(argv[1]) / (cents[1]);
+
+		if (r3 == 0)
+		{
+			printf("%i\n", r2);
+			return (1);
+		}
 	}
+
+	if (atoi(argv[1] <= (cents[0]);
+	{
+		r5 = atoi(argv[1]) % (cents[2]);
+		r6 = atoi(argv[1]) / (cents[2]);
+		if (r5 == 0)
+		{
+			printf("%i\n", r2);
+			return (1);
+		}
+	}
+
+	if (atoi(argv[1] <= (cents[0]);
+	{
+		p1 = atoi(argv[1]) % (cents[3]);
+		p2 = atoi(argv[1]) / (cents[3]);
+		if (p1 == 0)
+		{
+			printf("%i\n", r2);
+			return (1);
+		}
+	}
+
+	if (atoi(argv[1] <= (cents[0]);
+	{
+		p3 = atoi(argv[1]) % (cents[4]);
+		p4 = atoi(argv[1]) / (cents[4]);
+		if (p3 == 0)
+		{
+			printf("%i\n", r2);
+			return (1);
+		}
+	}
+
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,10 +19,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return(1);
 	}
+
 	for (i = 1; i < argc; i++)
 	{
 		product *= atoi(argv[i]);
 	}
+
 	printf("%d\n", product);
 	return (0);
 }
