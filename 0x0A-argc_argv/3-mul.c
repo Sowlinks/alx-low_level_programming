9524b1be9f7c75eb77c05f8ea52ca461078e5d2a
kurolinks <wsubiri@gmail.com>
2023-09-01 18:39:10 +0000

    edit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, product = 1;
+	int i, product, num1, num2;
 
 	if (argc != 3)
 	{
@@ -20,10 +20,9 @@ int main(int argc, char *argv[])
 		return(1);
 	}
 
-	for (i = 1; i < argc; i++)
-	{
-		product *= atoi(argv[i]);
-	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	product = num1 * num2;
 
 	printf("%d\n", product);
 	return (0);
