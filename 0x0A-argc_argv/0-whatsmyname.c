c09e32ce9effce12ad1748db9fcc3b5f861c2765
kurolinks <wsubiri@gmail.com>
2023-09-01 08:24:59 +0000

    add directory for argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- function to print name
+ * @argc: number of arguments
+ * @argv: array of stings
+ * Description: a program that prints its name
+ * Return: return name of program
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("The name of the program is: %s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- function to print arguments
+ * @argc: number of arguments
+ * @argv: array of stings
+ * Description: a function that prints the number of arguments passed into is
+ * Return: return number of args
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("The arg count is: %d\n", argc);
+}
