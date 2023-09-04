ad74266d8d8087667a3808b92a7ca7db2baebe1f
kurolinks <wsubiri@gmail.com>
2023-09-04 11:22:46 +0000

    add directory for memory allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+
+
+/**
+ * *create_array - fucntion to allocate memory
+ * @size: size of the datat type
+ * @c: character
+ * Description: a function that creates an array of chars and initialixes it with specific char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
