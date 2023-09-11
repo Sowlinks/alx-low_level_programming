5310f66ff1502ac0112f7f01335d861483e6b90b
kurolinks <wsubiri@gmail.com>
2023-09-11 17:56:48 +0000

    file 1 and 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints file name
+ * Description: program that prints the name of the file
+ * it was compiled from, followed by a new line.
+ * Return: returns void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
