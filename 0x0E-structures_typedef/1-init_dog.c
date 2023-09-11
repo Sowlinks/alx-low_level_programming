1855c5ef3f02156c669259783a328c7ca1a1296d
kurolinks <wsubiri@gmail.com>
2023-09-11 20:48:37 +0000

    add files

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize struct dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
