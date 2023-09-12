47f7cd808305abcd9c0cb702a605f22e5e789afa
kurolinks <wsubiri@gmail.com>
2023-09-12 17:41:49 +0000

    edit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,14 +16,13 @@ struct dog
 };
 
 /**
- * dog_t - new dog name
+ * dog_t - Typedef for struct dog
  */
-
-typedef dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
