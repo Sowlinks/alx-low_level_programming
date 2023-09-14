b4c8c8bb26ddee06cde45da4010aa9702a33b07e
kurolinks <wsubiri@gmail.com>
2023-09-14 04:57:42 +0000

    add directory for fucntion pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to name
+ * @f: function pointer
+ * Return: returns void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	if (name && f)
+			f(name);
+}
