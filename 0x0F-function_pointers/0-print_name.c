b456c2d1e512e5074d99944423993fe104db55fd
kurolinks <wsubiri@gmail.com>
2023-09-14 04:59:59 +0000

    file 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,8 +7,8 @@
  * Return: returns void
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-			f(name);
+		f(name);
 }
