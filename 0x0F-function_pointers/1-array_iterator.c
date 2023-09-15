54ba6d064a5dad4c2c0033ff1bc0ee9a779f0eed
kurolinks <wsubiri@gmail.com>
2023-09-15 07:09:47 +0000

    edit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,7 @@
  * Description: a function that executes a function given
  * as a parameter on each element of an array.
 */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int num;
